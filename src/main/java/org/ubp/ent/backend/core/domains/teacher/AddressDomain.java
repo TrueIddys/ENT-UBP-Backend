005ec0ef64c982a9bdb65f40c4a31895a266573c
anthonyraymond <raymond.anthony.63@hotmail.fr>
2016-01-11 18:16:01 +0100

    Added AddressDomain

---
+++ src/main/java/org/ubp/ent/backend/core/domains/teacher/AddressDomain.java
@@ -0,0 +1,67 @@
+package org.ubp.ent.backend.core.domains.teacher;
+
+import com.google.common.base.Objects;
+import org.ubp.ent.backend.core.domains.ModelTransformable;
+import org.ubp.ent.backend.core.model.teacher.Address;
+
+import javax.persistence.Embeddable;
+
+/**
+ * Created by Anthony on 11/01/2016.
+ */
+@Embeddable
+public class AddressDomain implements ModelTransformable<Address> {
+
+    private String streetNumber;
+    private String street;
+    private String zip;
+    private String city;
+
+    public AddressDomain() {
+    }
+
+    public AddressDomain(Address address) {
+        this.streetNumber = address.getStreetNumber();
+        this.street = address.getStreet();
+        this.zip = address.getZip();
+        this.city = address.getCity();
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public Address toModel() {
+        return new Address(streetNumber, street, zip, city);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AddressDomain that = (AddressDomain) o;
+        return Objects.equal(zip, that.zip) &&
+                Objects.equal(city, that.city) &&
+                Objects.equal(streetNumber, that.streetNumber) &&
+                Objects.equal(street, that.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(zip, city, streetNumber, street);
+    }
+
+}

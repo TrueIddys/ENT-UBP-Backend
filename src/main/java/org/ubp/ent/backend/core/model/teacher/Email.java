d1557f52f05d7a3d38a85a72afe9278dc0920f8b
anthonyraymond <raymond.anthony.63@hotmail.fr>
2016-01-13 01:01:28 +0100

    Added Email address

---
+++ src/main/java/org/ubp/ent/backend/core/model/teacher/Email.java
@@ -0,0 +1,49 @@
+package org.ubp.ent.backend.core.model.teacher;
+
+import com.google.common.base.Objects;
+import org.apache.commons.lang3.StringUtils;
+import org.ubp.ent.backend.core.exceptions.model.BadFormattedEmailAddress;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Anthony on 13/01/2016.
+ */
+public class Email {
+
+    private String address;
+
+    public Email(String address) {
+        if (StringUtils.isBlank(address)) {
+            throw new IllegalArgumentException("Cannot build a " + getClass().getName() + " without an email address");
+        }
+        validateAddress(address);
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    private void validateAddress(String mail) {
+        Pattern p = Pattern.compile("\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\b", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(mail);
+        if (!m.matches()) {
+            throw new BadFormattedEmailAddress("Given email address does not match pattern, given :" + mail);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Email email = (Email) o;
+        return Objects.equal(address, email.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(address);
+    }
+}
---
+++ src/test/java/org/ubp/ent/backend/core/model/teacher/EmailTest.java
@@ -0,0 +1,68 @@
+package org.ubp.ent.backend.core.model.teacher;
+
+import org.junit.Test;
+import org.ubp.ent.backend.core.exceptions.model.BadFormattedEmailAddress;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.StrictAssertions.fail;
+
+/**
+ * Created by Anthony on 13/01/2016.
+ */
+public class EmailTest {
+
+    private static final String VALID_EMAIL = "aaa-rrr@ent-ubp.fr";
+
+    public static Email createOne() {
+        return new Email(VALID_EMAIL);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotInstantiateWithNullNumber() {
+        new Email(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotInstantiateWithEmptyNumber() {
+        new Email(" ");
+    }
+
+    @Test
+    public void shouldInstantiate() {
+        Email model = new Email(VALID_EMAIL);
+        assertThat(model.getAddress()).isEqualTo(VALID_EMAIL);
+    }
+
+    @Test
+    public void shouldRejectNonPhoneNumber() {
+        List<String> wrongMails = Arrays.asList(
+                "abcdefghij",
+                "@ent-ubp.fr",
+                "aa@ent-ubp",
+                "aa@fr"
+        );
+        for (String mail : wrongMails) {
+            try {
+                new Email(mail);
+                fail("'" + mail + "' should not be a valid email address");
+            } catch (BadFormattedEmailAddress e) {
+                assertThat(e.getMessage()).isNotEmpty();
+            }
+        }
+    }
+
+
+    @Test
+    public void shouldBeEqualByNumber() {
+        assertThat(createOne()).isEqualTo(createOne());
+    }
+
+    @Test
+    public void shouldNotBeEqualWithDifferentNumbers() {
+        assertThat(createOne()).isNotEqualTo(new Email("pomme-api@ent-ubp.fr"));
+    }
+
+}
